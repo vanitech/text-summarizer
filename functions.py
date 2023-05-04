38316b7945d2dfb25690672ad6bc65114ec7fa45
vanitech <vanimandava@hotmail.com>
2023-05-04 16:40:18 -0700

    Update functions.py
    Added comments to describe temperature and max_tokens parameters

--- functions.py
+++ functions.py
@@ -7,8 +7,8 @@ def summarize(prompt):
         st.session_state["summary"] = openai.Completion.create(
             model="text-davinci-003",
             prompt=augmented_prompt,
-            temperature=.5,
-            max_tokens=1000,
+            temperature=.5, # temperature parameter is set between 0 (less random) to 2 (more random)
+            max_tokens=1000, # token count of the prompt plus max_tokens cannot exceed the model's context length (4096 for davinci-003)
         )["choices"][0]["text"]
     except:
         st.write('There was an error =(')
